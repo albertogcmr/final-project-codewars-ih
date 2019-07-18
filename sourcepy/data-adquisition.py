3fac726ec1b79637f572b3dfe4d9dd637c12e0be
albertogcmr <garcia.cobo.alberto@gmail.com>
2019-07-18 11:46:12 +0200

    13020 users

--- sourcepy/data-adquisition.py
+++ sourcepy/data-adquisition.py
@@ -7,11 +7,6 @@ MAX_USERS = 13005 # modificar
 OUTPUT_FILE_PATH = './output/codewar_users.csv'
 
 
-# TO DO: mover esta función CWData como método y llamarlo desde el main
-
-    
-
-
 def main(): 
     top500 = get_leaderboard_users()
     data = CWData(users_seed=top500, seed_path=OUTPUT_FILE_PATH, max_users=MAX_USERS)
