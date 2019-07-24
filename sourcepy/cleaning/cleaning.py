9919f553b4c27eead5823a0265b54585b6e93831
albertogcmr <garcia.cobo.alberto@gmail.com>
2019-07-24 20:06:31 +0200

    data-cleaning.py

--- sourcepy/cleaning/cleaning.py
+++ sourcepy/cleaning/cleaning.py
@@ -1,8 +1,25 @@
+# imports 
+
+import pandas as pd
 import re
+
+
+def get_numeric_one(x, deletes=''): 
+    try: 
+        num = str(x)
+        for ch in deletes: 
+            num = num.replace(ch, '').strip()
+        return float(num)
+    except: 
+        return x
+
 
 def get_numeric_groups(x, groups=1): 
     try: 
         numbers = [int(number) for number in re.findall(r'\d+', x)]
+        # in case x=0 or x=='0'
+        if str(x) == '0': 
+            numbers = [0] * groups
     except: 
         numbers = [0] * groups
     else: 
@@ -10,17 +27,88 @@ def get_numeric_groups(x, groups=1):
     finally: 
         # return list if groups > 1 else return first element in list
         return numbers[:groups] if groups > 1 else numbers[0]
-# df["authored_translations"], df["approved_translations"] = zip(*df["authored translations"].map(lambda x: get_numeric_groups(x, groups=2)))
+    
+def get_highest_trained(x): 
+    try: 
+        res = x.split('(')[0].strip()
+        return res
+    except: 
+        return x
+    
+def get_honor_percentile(x): 
+    try: 
+        res = x.replace('Top', '').replace('%', '').strip()
+        return float(res)
+    except: 
+        return x
 
 
 def cleaning(df): 
+    # clean column names
+    df.columns = df.columns.str.replace(' ', '_').str.replace('.', '').str.lower()
 
-    columns = ["authored_translations", "approved_translations", "avg_satisfaction_rating", 'best_practice', 
-                'best_practice_solutions']
-    for col in columns: 
-        print(df[col].unique())
+    # replace nan in numeric colmuns with 0
+    df._get_numeric_data().fillna(0, inplace=True)
 
 
+    # creates two new columns and delete the previous
+    df["authored_translations"], df["approved_translations"] = zip(*df["authored_translations"].map(lambda x: get_numeric_groups(x, groups=2)))
+    df["authored_translations"].value_counts()
+
+    # creates new column and delete the previous
+    df['avg_rank'] = df['avg_rank'].apply(lambda x: get_numeric_one(x, deletes='kyudan')).fillna(0)
+
+    # creates new column and delete the previous
+    df['avg_satisfaction_rating'] = df['avg_satisfaction_rating'].apply(lambda x: get_numeric_one(x, deletes='%')).fillna(0)
+
+
+    # creates new column and delete the previous
+    df['best_practice'] = df['best_practice'].apply(get_numeric_one).fillna(0)
+
+
+    # creates new column and delete the previous
+    df['best_practice_solutions'] = df['best_practice_solutions'].apply(lambda x: get_numeric_one(x, deletes=',')).fillna(0)
+
+
+    # creates new column and delete the previous
+    df['clever_solutions'] = df['clever_solutions'].apply(lambda x: get_numeric_one(x, deletes=',')).fillna(0)
+
+    # creates two new columns and delete the previous
+    df["comments"], df["replies"] = zip(*df["comments"].map(lambda x: get_numeric_groups(x, groups=2)))
+
+    # creates two new columns and delete the previous
+    df["created"], df["beta"] = zip(*df["created"].map(lambda x: get_numeric_groups(x, groups=2)))
+
+    # cleans dates column
+    df['date'] = pd.to_datetime(df['date'])
+    df['ended_on'] = pd.to_datetime(df['ended_on'])
+    df['first_completed'] = pd.to_datetime(df['first_completed'])
+    df['last_completed'] = pd.to_datetime(df['last_completed'])
+    df['last_seen'] = pd.to_datetime(df['last_seen'])
+    df['member_since'] = pd.to_datetime(df['member_since'])
+
+    # creates new column and delete the previous
+    df['highest_trained'] = df['highest_trained'].apply(get_highest_trained)
+
+    # creates new column and delete the previous
+    df['honor'] = df['honor'].apply(lambda x: get_numeric_one(x, deletes=',')).fillna(0)
+
+    # creates new column and delete the previous
+    df['honor_percentile'] = df['honor_percentile'].apply(lambda x: get_numeric_one(x, deletes='Top%')).fillna(0)
+
+    # creates two new columns and delete the previous
+    df["kumite"], df["started_kumite"] = zip(*df["kumite"].map(lambda x: get_numeric_groups(x, groups=2)))
+
+    # creates new column and delete the previous
+    df['leaderboard_position'] = df['leaderboard_position'].apply(lambda x: get_numeric_one(x, deletes='#')).fillna(0)
+
+    # creates new column and delete the previous
+    df['rank'] = df['rank'].apply(lambda x: get_numeric_one(x, deletes='kyudan')).fillna(0)
+
+    # creates new column and delete the previous
+    df['total_collected'] = df['total_collected'].apply(get_numeric_one).fillna(0)
+
+    # creates two new columns and delete the previous
+    df["translations"], df["translations_aproved"] = zip(*df["translations"].map(lambda x: get_numeric_groups(x, groups=2)))
 
     return df
-
--- sourcepy/data-cleaning.py
+++ sourcepy/data-cleaning.py
@@ -12,12 +12,14 @@ from acquisition.languages import get_languages
 from cleaning.cleaning import cleaning
 
 INPUT_FILE_PATH = './output/codewar_users.csv' 
+OUTPUT_FILE_PATH = './output/codewar_users_clean.csv' 
 
 
 def main(): 
     df = pd.read_csv(INPUT_FILE_PATH, index_col=0)
     df = cleaning(df)
-    
+    df.to_csv(OUTPUT_FILE_PATH)
+
 
 
 
